3bb37a4c80df3f3c4fc9333f422e98a033ce829a
Lucas Gut <lucaschueko@yahoo.es>
2019-03-24 20:28:56 +0000

    Do not auto-complete for empty search string.

--- src/greenbow/company-search.cljs
+++ src/greenbow/company-search.cljs
@@ -8,9 +8,12 @@
            (filter #(.contains (s/lower-case (:name %)) (s/lower-case search-string)) companies)))
 
 (defn- find-establishment-keywords-filter [keyword search-string]
-  (if (> (count search-string) 1)
-    (.contains (s/lower-case keyword) (s/lower-case search-string))
-    (.startsWith (s/lower-case keyword) (s/lower-case search-string))))
+  (let [search-string-length (count search-string)]
+    (if (= search-string-length 0)
+      false
+      (if (> search-string-length 1)
+        (.contains (s/lower-case keyword) (s/lower-case search-string))
+        (.startsWith (s/lower-case keyword) (s/lower-case search-string))))))
 
 ;; Filter the specified list of establishment keywords
 (defn find-establishment-keywords [establishment-keywords search-string]
